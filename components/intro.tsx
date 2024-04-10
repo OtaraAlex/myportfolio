3c67dae864c8f338e5c5d973df7a2d1d3aa246f7
Alex Otara <mbechealex61@gmail.com>
2024-04-10 12:30:14 +0300

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1mqtwFXULx1sQ-KV7nW09qs5kE5TrDOKg/view?usp=sharing"
+          href="https://drive.google.com/file/d/1SsqsknXHtP-T-ftiY9krBemSTG5E8LdL/view?usp=sharing"
           target="_blank"
         >
           View Resume
